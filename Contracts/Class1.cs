accbdd0a72221fbf0664a50568971af86cf4273d
wesam fatafta <wiasm2345wiasm@gmail.com>
2025-07-24 21:57:55 +0300

    Add files via upload

---
+++ Contracts/Class1.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Contracts
+{
+    public interface IFoodServise
+    {
+      
+        public void CountFood();
+        public void CountDrink();
+        public void CountOverall();
+        public  void MaxPrice();
+        public int MaxNumberOfRequests();
+        public void MinPrice();
+        public int  MinNumberOfRequests();
+        public void PrintAll();
+        public void AvgPrice();
+       
+        public void SearchById(int id);
+        public void GetMostPopularItem();
+    }
+    public class FoodDto { 
+    public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDrink { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfRequests { get; set; }
+        public void PrintInfo() {
+            Console.WriteLine($"Id:{Id},Name:{Name},Price:{Price},Number of requests:{NumberOfRequests}");
+        }
+    }
+}
---
+++ Domin/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Domin
+{
+    public class Food
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsDrink { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfRequests { get; set; }
+
+    }
+}
